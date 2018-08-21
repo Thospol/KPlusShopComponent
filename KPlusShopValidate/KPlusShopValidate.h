2b3ba65be978abd78abadba55d8313581f445c10
thosapol <isocare.thospol@gmail.com>
2018-08-21 12:59:46 +0700

    Initial Commit

---
+++ KPlusShopValidate/KPlusShopValidate.h
@@ -0,0 +1,19 @@
+//
+//  KPlusShopValidate.h
+//  KPlusShopValidate
+//
+//  Created by thospol on 21/8/61.
+//  Copyright © พ.ศ. 2561 thospol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KPlusShopValidate.
+FOUNDATION_EXPORT double KPlusShopValidateVersionNumber;
+
+//! Project version string for KPlusShopValidate.
+FOUNDATION_EXPORT const unsigned char KPlusShopValidateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KPlusShopValidate/PublicHeader.h>
+
+
